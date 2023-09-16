bd31cb36273c8247527fe8736649ea0b0a106618
yongun2 <djkye0001@naver.com>
2023-09-16 23:11:29 +0900

    [FEAT] 통일된 Response 형태

---
+++ src/main/java/com/hongik/kiosk/ui/view/ApiResponseView.java
@@ -0,0 +1,16 @@
+package com.hongik.kiosk.ui.view;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponseView<T> {
+    private final T data;
+
+    public ApiResponseView(T data) {
+        this.data = data;
+    }
+}
