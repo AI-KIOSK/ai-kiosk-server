eaf7fd8f630a454b968ca08e9826a28d4e664cf6
yongun2 <djkye0001@naver.com>
2023-11-20 10:17:15 +0900

    [UPDATE] compact view 카테고리 추가

--- src/main/java/com/hongik/kiosk/ui/view/MenuCompactView.java
+++ src/main/java/com/hongik/kiosk/ui/view/MenuCompactView.java
@@ -2,6 +2,7 @@ package com.hongik.kiosk.ui.view;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.hongik.kiosk.application.service.menu.MenuReadUseCase;
+import com.hongik.kiosk.infrastructure.persistence.postgresql.entity.CategoryEntity;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -11,12 +12,15 @@ import lombok.ToString;
 public class MenuCompactView {
     private final Long id;
     private final String name;
+    private final String category;
+
     private final String iceImgUrl;
     private final String hotImgUrl;
 
     public MenuCompactView(MenuReadUseCase.FindMenuResult result) {
         this.id = result.getId();
         this.name = result.getName();
+        this.category = result.getCategory().getCategory();
         this.iceImgUrl = result.getIce_img_url();
         this.hotImgUrl = result.getHot_img_url();
     }
